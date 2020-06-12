4c898c499a03fa653920db0ef5f1b7f3b98adc28
sou kimura <nattoukimuti0201@gmail.com>
2020-06-12 11:42:30 +0900

    2nd files commit...

--- index.js
+++
@@ -1,5 +0,0 @@
-// indexからのPOSTでvalue取得
-// 取得したValueからどのJsonを取得するか判断
-// データ取得後、ページ遷移
-
-//post value json redirect
---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+// indexからのPOSTでvalue取得
+// 取得したValueからどのJsonを取得するか判断
+// データ取得後、ページ遷移
+
+//post value json redirect
+
+let elem_q_Btncategory = document.getElementById("qBtnCategory1");
+
+console.log(elem_q_Btncategory);
+
+elem_q_Btncategory.eventlistener('click'function(){
+
+});
+
+function getValue (elem){
+};
+
+function getQuestion(value){
+
+};
+
+function makeQuestion(value){
+
+};
+
+function cheResult(){
+
+};
+
+function sendPage(url){
+
+};
+
