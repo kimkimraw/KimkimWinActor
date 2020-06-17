1ae86d8559611e29b4982ad4976a422d3345a119
sou kimura <nattoukimuti0201@gmail.com>
2020-06-18 00:32:16 +0900

    main page viewUP

--- public/js/makeQuestion.js
+++ public/js/makeQuestion.js
@@ -1,113 +1,40 @@
-// Jsonを使用した処理
+// question.html のクエリパラメータを受け取ってView作成する
 
-// indexからのget form quetyparam
-// 取得したparamからどのJsonを取得するか判断
-// onloadでview作成（読み込み画面作成しても良いかも）
-
-// html button -> button value = json.qNo -> for makequestion
- // -> page to question.html  
-
-
-
-// クエリパラメーた　が
-// q＿category=0　の時
-
-// jsonはquestions[0].q_category1[0]
-// をとる
-
-// 具体的には
-// 1 スイッチ分で分岐する
-// 2 クエリパラメータがnの時Jsonは●●を取得する
-// 3 取得したJsonをHTMlに処理、Viewする
-
-// nn error はおいとく
-
-//post value json redirect
-console.log("get makeQuestion.js!!");
 // get quetyParam
 let param = location.search;
-console.log('quetyParam :' + param);
-alert(param);
-
-
-// get container ID Null
-let containerElem = document.getElementById('container');
-
-
-// json test
-console.log(questions);
-console.log(questions[0].q_category1[0].q_Titile);
+// get ID
+let contentElem = document.getElementById('content');
 // create view
 let html = createQuestion(questions);
-console.log(html);
-
+// クエリパラメータの処理
 switch (param){
   case '?qCategory=0':
-  	// containerElem.insertAdjacentHTML('beforeend', html.join(''));
-  	document.write(html);
+  	contentElem.insertAdjacentHTML('afterbegin',html);
     break;
   case '?qCategory=1':
-    alert('ok');
+    alert('未実装');
     break;
   case '?qCategory=2':
-    alert('ok');
+    alert('未実装');
     break;
   case '?qCategory=3':
-  	alert('ok');
+  	alert('未実装');
   	break;
    default:
-      document.write("fuck you!!<br />");
+      document.write("TOPからやり直しな小僧!!<br />");
       break; 
 };
-
-// //get html button value
-// function getValue (elem){
-// 	let value = elem.value;
-// 	console.log("BTN_VALUE :" +  elem.value);
-// 	// button is undefined error
-// 	if (value == undefined) {
-// 		alert('予期しないエラーが発生しました。');
-// 		return false
-// 	}
-// 	//return button value 
-// 	return value
-// };
-
-// //check -> button value == JsonSectionNo 
-// function IsJsonName(elem,jsonNum){
-// 	// get btn value 
-// 	let btnValue = getValue(elem);
-// 	// return true
-// 	if(btnValue == jsonNum){
-// 		return true;
-// 	// return false is error
-// 	}else{
-// 		alert("failre....");
-// 		return false;
-// 	}
-// };
-
 //create question view
 function createQuestion(arrays){
 	let html = [];
-	html.push('<form name="user_answer" action="" method="POST">');
-	for (let i=0; i < 1;i++){
-		html.push('<h2>' + arrays[0].q_category1[i].q_Titile + '</h2>'
-			+'<p>' + arrays[0].q_category1[i].q_Select1 + '</p>');
-	}
-	html.push('</form>');
-
+	html.push('<div>');
+	for (let i=0; i < 2;i++){
+		html.push('<form name="user_answer" action="./result.html" method="POST"><h3>' + arrays[0].q_category1[i].q_Titile + '</h3>'
+			+ '<input type="radio" name="U_Answer' + i + '"value=' + arrays[0].q_category1[i].q_Select1[1] + '>' + arrays[0].q_category1[i].q_Select1[0]
+			+ '<br><input type="radio" name="U_Answer' + i + '"value=' + arrays[0].q_category1[i].q_Select2[1] + '>' + arrays[0].q_category1[i].q_Select2[0]
+			+ '<br><input type="radio" name="U_Answer' + i + '"value=' + arrays[0].q_category1[i].q_Select3[1] + '>' + arrays[0].q_category1[i].q_Select3[0]
+			+ '<br><input type="radio" name="U_Answer' + i + '"value=' + arrays[0].q_category1[i].q_Select4[1] + '>' + arrays[0].q_category1[i].q_Select4[0]
+			+ '</form></div>'
+	);};
 	return html
-};
-
-
-
-
-
-function cheResult(){
-
-};
-
-function sendPage(url){
-
 };
--- public/js/slick.js
+++ public/js/slick.js
@@ -5,30 +5,3 @@ $(".single-item").slick({
   dots: true,
   fade: true,
 });
-
-
-// $('.center').slick({
-//   centerMode: true,
-//   centerPadding: '60px',
-//   slidesToShow: 3,
-//   responsive: [
-//     {
-//       breakpoint: 768,
-//       settings: {
-//         arrows: false,
-//         centerMode: true,
-//         centerPadding: '40px',
-//         slidesToShow: 3
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         arrows: false,
-//         centerMode: true,
-//         centerPadding: '40px',
-//         slidesToShow: 1
-//       }
-//     }
-//   ]
-// });
